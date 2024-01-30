92f7e685c36978c05c77b2440c547df9385935e4
alexmortelsison <alexmortelsison@gmail.com>
2024-01-31 08:58:45 +0900

    Initial Commit

---
+++ Head Or Tail/Head_Or_TailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Head_Or_TailApp.swift
+//  Head Or Tail
+//
+//  Created by Alex Sison on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Head_Or_TailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
